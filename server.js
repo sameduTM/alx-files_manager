4ae55593c42832efaaaa89e5a21ff58d6f2b7048
Ken Wekesa <wanyamak884@gmail.com>
2025-09-23 03:07:03 +0300

    server.js - added Missing semicolon

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import allRoutes from './routes/index';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(allRoutes);
 
