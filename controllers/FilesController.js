6290e1ffc529dc83cf2c64c93310994bbbc34c18
Ken Wekesa <wanyamak884@gmail.com>
2025-09-28 12:29:13 +0300

    re-written FileController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,26 +1,32 @@
-import { v4 as uuidv4 } from 'uuid';
+import pkg from 'mongodb';
 import { writeFileSync, existsSync, mkdirSync } from 'fs';
-import { ObjectID } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
+const { ObjectId } = pkg;
+
 class FilesController {
   static async postUpload(request, response) {
-    const token = request.get('X-Token');
-    const userId = await redisClient.get(`auth_${token}`);
+    const tokenId = request.get('X-Token');
+    const userId = await redisClient.get(`auth_${tokenId}`);
 
     if (!userId) {
       return response.status(401).json({ error: 'Unauthorized' });
     }
 
     const {
-      name, type, parentId, data, isPublic,
+      name, type, data,
     } = request.body;
+
+    const isPublic = request.body.isPublic || false;
+
+    let parentId = request.body.parentId || 0;
 
     if (!name) {
       return response.status(400).json({ error: 'Missing name' });
     }
-    if (!type || !['folder', 'file', 'image'].includes(type)) {
+    if (!type) {
       return response.status(400).json({ error: 'Missing type' });
     }
     if (!data && type !== 'folder') {
@@ -29,65 +35,63 @@ class FilesController {
     if (parentId) {
       const file = await dbClient.getFileById(parentId);
 
-      if (!file || file.length === 0) {
+      if (!file[0]) {
         return response.status(400).json({ error: 'Parent not found' });
       }
-      if (file[0].type !== 'folder') {
+      if (!file[0] && type !== 'folder') {
         return response.status(400).json({ error: 'Parent is not a folder' });
       }
+      parentId = ObjectId(parentId);
     }
-
     if (type === 'folder') {
-      const obj = {
-        userId: new ObjectID(userId),
+      const newFolder = {
+        userId: ObjectId(userId),
         name,
         type,
         isPublic: isPublic || false,
-        parentId: new ObjectID(parentId) || 0,
+        parentId: parentId || 0,
       };
-      const doc = await dbClient.createFile(obj);
-      const newFileAttr = {
-        id: doc.ops[0]._id,
+      const addedFolder = await dbClient.createFile(newFolder);
+
+      return response.status(201).json({
+        id: addedFolder.insertedId,
         userId,
         name,
         type,
-        isPublic: doc.ops[0].isPublic,
-        parentId: doc.ops[0].parentId,
-      };
-
-      return response.status(201).json(newFileAttr);
+        isPublic: addedFolder.ops[0].isPublic,
+        parentId: addedFolder.ops[0].parentId,
+      });
     }
 
-    const filePath = process.env.FILE_PATH || '/tmp/files_manager';
-    const localPath = `${filePath}/${uuidv4()}`;
+    const relativePath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const localPath = `${relativePath}/${uuidv4()}`;
     const decodedData = Buffer.from(data, 'base64');
 
-    if (!existsSync(localPath)) {
-      mkdirSync(filePath, { recursive: true });
+    if (!existsSync(relativePath)) {
+      mkdirSync(relativePath, { recursive: true });
     }
 
     writeFileSync(localPath, decodedData);
 
-    const fileAttributes = {
-      userId: new ObjectID(userId),
+    const newFileDocument = {
+      userId: ObjectId(userId),
       name,
       type,
       isPublic: isPublic || false,
-      parentId: new ObjectID(parentId) || 0,
+      parentId: parentId || 0,
       localPath,
     };
-    const fileDoc = await dbClient.createFile(fileAttributes);
 
-    const newFileAttr = {
-      id: fileDoc.ops[0]._id,
+    const addedFile = await dbClient.createFile(newFileDocument);
+
+    return response.status(201).json({
+      id: addedFile.insertedId,
       userId,
       name,
       type,
-      isPublic: fileDoc.ops[0].isPublic,
-      parentId: fileDoc.ops[0].parentId,
-    };
-
-    return response.status(201).json(newFileAttr);
+      isPublic: addedFile.ops[0].isPublic,
+      parentId: addedFile.ops[0].parentId,
+    });
   }
 }
 
